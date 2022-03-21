1fd3dc7bf099e79252c7fa823d543f6dadabbe4e
swetaraj4194 <swetaraj4194@gmail.com>
2022-03-21 16:44:05 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar";
+// import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">HEllo doing some setup</div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>HEllo doing some setup</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div>
+      <NavLink to="">The Shop </NavLink>
+      <NavLink to="">Home</NavLink>
+      <NavLink to="">Shop</NavLink>
+      <NavLink to="">About</NavLink>
+    </div>
+  );
+}
