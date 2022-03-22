f444ac223811c8bcd2b45c1cee6aed876c377c60
Alina-Kovaleva <a.d.kovaleva@yandex.com>
2022-03-22 09:55:16 +0100

    added styles for navBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,74 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./styles.css";
 
 function NavBar() {
   return (
     <div className="navbar-container">
-      {/* <NavLink to="">The Shop </NavLink>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/shop">Shop</NavLink>
-      <NavLink to="/details/1">About</NavLink> */}
+      <div className="nav-row left">
+        <div className="container">
+          <NavLink to="/" className="nav-link nav-logo">
+            The<span className="right-logo">Shop</span>
+          </NavLink>
+          <NavLink
+            to="/"
+            className="nav-link"
+            style={({ isActive }) =>
+              isActive ? { color: "white" } : undefined
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/shop"
+            className="nav-link"
+            style={({ isActive }) =>
+              isActive ? { color: "white" } : undefined
+            }
+          >
+            Products
+          </NavLink>
+          <NavLink
+            to="/"
+            className="nav-link"
+            style={({ isActive }) =>
+              isActive ? { color: "white" } : undefined
+            }
+          >
+            Blog
+          </NavLink>
+          <NavLink
+            to="/shop"
+            className="nav-link"
+            style={({ isActive }) =>
+              isActive ? { color: "white" } : undefined
+            }
+          >
+            Shop
+          </NavLink>
+          <NavLink
+            to="/details/1"
+            className="nav-link"
+            style={({ isActive }) =>
+              isActive ? { color: "white" } : undefined
+            }
+          >
+            Contact
+          </NavLink>
+        </div>
+        <div>
+          <form>
+            <input className="nav-search" type="text" name="search"></input>
+          </form>
+        </div>
+      </div>
+      <div className="nav-row right">
+        <div className="login icon"></div>
+        {/* ^ should seend us to login page */}
+        <div className="cart icon"></div>
+        {/* ^ shouls seend us to  shoping cart page*/}
+        <div className="favorite icon"></div>
+        {/* ^ should seend us to favorites page */}
+      </div>
     </div>
   );
 }
